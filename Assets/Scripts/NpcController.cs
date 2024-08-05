52c3d7fca88dec1a824e241e9383222458157329
Loupzy_Dev <eduardo.posadas@saei.mx>
2024-08-04 23:21:55 -0600

    Intento

--- Assets/Scripts/NpcController.cs
+++ Assets/Scripts/NpcController.cs
@@ -1,153 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 
 public enum NpcState {
-    Quiet,
-    Working,
-    Resting
+    None,
+    Playing,
+    Working
 }
 
 public class NpcController : Agent {
-    NpcState state;
+    private NpcState state;
+    private Rigidbody rb;
+    [SerializeField] private float moveSpeed = 5f;
 
-    [SerializeField] float stamina = 10f;
-    [SerializeField] float health = 10f;
-
-    [SerializeField] bool isWorking = false;
-    [SerializeField] bool isTired = false;
-    [SerializeField] bool isRested = false;
-
-    private float staminaDecreaseRate = 1f;
-    private float staminaIncreaseRate = 1f;
-    private float healthDecreaseRate = 0.5f;
+    public override void Initialize() {
+        rb = GetComponent<Rigidbody>();
+        ChangeGameState(NpcState.None); 
+    }
 
     public void ChangeGameState(NpcState newState) {
         state = newState;
         switch (state) {
-            case NpcState.Quiet:
-                isWorking = false;
-                isRested = false;
-                isTired = false;
+            case NpcState.None:
+                break;
+            case NpcState.Playing:
+                Debug.Log("P");
                 break;
             case NpcState.Working:
-                isWorking = true;
-                isRested = false;
-                isTired = false;
-                break;
-            case NpcState.Resting:
-                isWorking = false;
-                isRested = true;
-                isTired = false;
+                Debug.Log("W");
                 break;
         }
-    }
-
-    private void Update() {
-        Debug.Log(state.ToString());
     }
 
     public override void OnEpisodeBegin() {
-        state = NpcState.Working;
-        stamina = 10f;
-        health = 10f;
-    }
-
-    public override void OnActionReceived(ActionBuffers actions) {
-        switch (actions.DiscreteActions[0]) {
-            case 0:
-                ChangeGameState(NpcState.Quiet);
-                break;
-            case 1:
-                ChangeGameState(NpcState.Working);
-                break;
-            case 2:
-                ChangeGameState(NpcState.Resting);
-                break;
-        }
-
-        switch (state) {
-            case NpcState.Working:
-                Work();
-                break;
-            case NpcState.Resting:
-                Rest();
-                break;
-            case NpcState.Quiet:
-                break;
-        }
-
-        AddReward(CalculateReward());
+        ChangeGameState(NpcState.None);
     }
 
     public override void CollectObservations(VectorSensor sensor) {
-        sensor.AddObservation(stamina);
-        sensor.AddObservation(health);
-        sensor.AddObservation((int)state);
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(rb.velocity); 
+    }
+
+    public override void OnActionReceived(ActionBuffers actions) {
+        float moveRotate = actions.ContinuousActions[0];
+        float moveForward = actions.ContinuousActions[1];
+
+        rb.MovePosition(transform.position + transform.forward * moveForward * moveSpeed * Time.deltaTime);
+        transform.Rotate(0f, moveRotate * moveSpeed, 0f, Space.Self);
     }
 
     public override void Heuristic(in ActionBuffers actionsOut) {
-        var discreteActionsOut = actionsOut.DiscreteActions;
-        discreteActionsOut[0] = (int)state;
-    }
-
-    private void Work() {
-        if (isWorking) {
-            stamina -= staminaDecreaseRate * Time.deltaTime;
-            if (stamina < 2f) {
-                health -= healthDecreaseRate * Time.deltaTime;
-            }
-            if (stamina <= 0) {
-                stamina = 0;
-                ChangeGameState(NpcState.Resting);
-            }
-        }
-    }
-
-    private void Rest() {
-        if (isRested) {
-            stamina += staminaIncreaseRate * Time.deltaTime;
-            if (stamina >= 10f) {
-                stamina = 10f;
-                ChangeGameState(NpcState.Quiet);
-            }
-        }
-    }
-
-    private float CalculateReward() {
-        float reward = 0f;
-
-        if (health <= 0) {
-            reward -= 20f;  
-            EndEpisode();   
-        } else {
-            if (state == NpcState.Working) {
-                if (stamina > 9f) {
-                    reward += 10f;  
-                } else if (stamina > 7f) {
-                    reward += 0.4f;  
-                } else if (stamina > 2f) {
-                    reward += 0.1f;  
-                }
-            }
-
-            if (state == NpcState.Resting && stamina < 10f) {
-                reward += 0.05f;
-            }
-
-            if (state == NpcState.Working && stamina <= 2f) {
-                reward -= 0.1f;
-            }
-        }
-
-        return reward;
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxis("Horizontal");
+        continuousActions[1] = Input.GetAxis("Vertical");
     }
 
     public void ChangeStateFromButton(int newState) {
         ChangeGameState((NpcState)newState);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Work")) {
+            if (state == NpcState.Working) {
+                AddReward(10f);
+                Debug.Log("Trabajando");
+            } else {
+                AddReward(-15f);
+            }
+        }
+        if (collision.gameObject.CompareTag("Fun")) {
+            if (state == NpcState.Playing) {
+                AddReward(10f);
+                Debug.Log("Jugando");
+            } else {
+                AddReward(-15f);
+            }
+        }
+        if (collision.gameObject.CompareTag("Wall")) {
+            AddReward(-15f);
+            EndEpisode();
+        }
     }
 }
